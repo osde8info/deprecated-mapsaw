f24db576019fea5d78e4d0d5e87c5e2eeef3a178
clive darra <cdarra@linux-qyz5.site>
2013-05-13 21:56:46 +0100

    My changes

--- php/private/gan.js
+++ php/private/gan.js
@@ -4,7 +4,7 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-747268-24', 'osde8info.com');
+  ga('create', 'UA-', '.com');
   ga('send', 'pageview');
 
 </script>
